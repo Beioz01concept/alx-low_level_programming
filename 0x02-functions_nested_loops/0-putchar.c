b7cef7697fdda1396baf19edc15b8e5bb5b6e1e5
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 <Izubunduwokoma247@gmail.com>
2023-12-20 16:58:03 -0800

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * main -Entry point of my code
+ * main - Entry point 
  *
- * Return: 0(success)
+ * Return: 0 (success)
  */
 
 int main(void);
 {
+
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -17,5 +18,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0):
+return (0);
 }
+
