5dcc4ec78c1ccbfae8599744522c4c0029d59034
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 <Izubunduwokoma247@gmail.com>
2023-12-22 09:00:14 -0800

    the mulTiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* mul - Do the multiplicatiion of two integers
+* @a: Function parameter 1
+* @b: Function parameter 2
+* Return: return void
+*/
+
+int mul(int a, int b)
+{
+
+return (a * b);
+
+}
