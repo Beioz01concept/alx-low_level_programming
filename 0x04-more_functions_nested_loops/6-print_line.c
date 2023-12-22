f4e2a7f899eb4881eb8060d4a3d5e9ae4fdcd0a8
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 <Izubunduwokoma247@gmail.com>
2023-12-22 13:49:41 -0800

    printing new line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal
+ * @n: Number of times the character '_' should be printed
+ *Return: 0(void)
+ */
+void print_line(int n)
+{
+	if (n > 0)
+{
+
+	_putchar('\n');
+	}
+	else
+	{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+
+	_putchar('\n');
+	}
+}
+
